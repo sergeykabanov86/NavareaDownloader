c4e99d0a371c2d05c029034958a445baf769a2e3
Sergey <Serge@KSE-DESKTOP>
2025-08-04 13:14:12 +0200

    Начинаю добавлять подключение к db

---
+++ Db/connection.py
@@ -0,0 +1,16 @@
+﻿from settings import host, password, user
+
+def create_connection():
+    print(f"user: {user} password: {password} host:{host}")
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    dir()
+    print()
+   #create_connection()
---
+++ Db/settings.py
@@ -0,0 +1,3 @@
+﻿user = ''
+password = ''
+host = ''
